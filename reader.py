476892e40022ba75ff1109c0ef9b29db50f87d84
ricciric <riki.marasca@gmail.com>
2024-09-06 17:27:44 +0200

    reader and video encoder

--- reader.py
+++ reader.py
@@ -1,10 +1,34 @@
 import numpy as np
+import cv2 as cv
 
 from image import Image
 
 class VideoReader:
     def __init__(self, path: str) -> None:
-        pass
+        
+        self.path= path
+        self.frames = []
+        self.video = self.load_video(path)
+        self.current_frame = 0
     
-    def next_frame() -> Image:
-        pass
+    def next_frame(self) -> Image:
+        frame = self.frames[self.current_frame]
+        self.current_frame += 1
+        return Image(frame)
+    
+    def load_video(self):
+        self.video = cv.VideoCapture(self.path)
+        self.extract_frames()
+    
+    def extract_frames(self):
+        while True:
+            state, frame = self.video.read()
+            if not state:
+                break
+            self.frames.append(frame)
+            
+    def get_total_frames(self):
+        return len(self.frames)
+            
+            
+        
--- video_encoder.py
+++ video_encoder.py
@@ -4,14 +4,42 @@ from transmitter import Transmitter
 from reader import VideoReader
 
 class VideoEncoder:
-    def __init__(self) -> None:
+    def __init__(self, path: str) -> None:
+        self.path = path
         self.encoder = Encoder()
         self.transmitter = Transmitter()
-        self.reader = VideoReader()
+        self.reader = VideoReader(path)
 
-    def encode_video(path: str) -> None:
+    def encode_video(self) -> None:
+        
+        num_frames = self.reader.get_total_frames()
         tmp_b1 = None
         tmp_b2 = None
+        
+        for i in range(num_frames):
+            frame = self.reader.next_frame()
+            
+            # Intra Frame
+            if i == 0 or (i % 9 == 0):
+                intra_frame = self.encoder.encode_intra_frame(frame)
+                self.transmitter.send_frame(intra_frame)
+                
+            # Predicted Frame - Bidirectional Frame
+            elif i % 3 == 0:
+                predicted_frame = self.encoder.encode_predicted_frame(frame)
+                bidirectional_frame1 = self.encoder.encode_bidirectional_frame(tmp_b1)
+                bidirectional_frame2 = self.encoder.encode_bidirectional_frame(tmp_b2)
 
-    def process_frame():
-        pass
+                self.transmitter.send_frame(bidirectional_frame1)
+                self.transmitter.send_frame(bidirectional_frame2)
+                self.transmitter.send_frame(predicted_frame)
+                
+                tmp_b1 = None
+                tmp_b2 = None
+                
+            else:
+                if tmp_b1 is None:
+                    tmp_b1 = frame
+                else:
+                    tmp_b2 = frame
+        
