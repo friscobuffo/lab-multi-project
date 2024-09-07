79e7cf680ee361cbf4914643195e4cf0756fcc07
friscobuffo <gio.andreola@stud.uniroma3.it>
2024-09-07 16:23:27 +0200

    receiver transmitter

--- receiver.py
+++ receiver.py
@@ -1,7 +1,22 @@
-class Receiver:
-    # inizializza socket
-    def __init__(self) -> None:
-        pass
+import socket
+import pickle
 
-    def receive():
-        pass
+class EndOfStream:
+    pass
+
+def receive(handle_data: function, close_function: function = None) -> None:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('localhost', 12345))
+    s.listen(1)
+    conn, addr = s.accept()
+    print('Connessione accettata da', addr)
+
+    while True:
+        data = conn.recv(1024)
+        unpacked_data = pickle.loads(data)
+        if (isinstance(unpacked_data, EndOfStream)):
+            conn.close()
+            if close_function:
+                close_function()
+            return
+        handle_data(unpacked_data)
--- transmitter.py
+++ transmitter.py
@@ -1,7 +1,14 @@
-class Transmitter:
-    # inizializza socket
-    def __init__(self) -> None:
-        pass
+import socket
+import pickle
 
-    def send():
-        pass
+class Transmitter:
+    def __init__(self) -> None:
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect(('localhost', 12345))
+
+    def send(self, object) -> None:
+        message = pickle.dumps(object)
+        self.s.sendall(message)
+    
+    def close(self) -> None:
+        self.s.close()
