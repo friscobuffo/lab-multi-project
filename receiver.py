07613d23c86e0cb1cf36f571e2bccc6c6391e4c9
Simone-Albero <simone.albero29@gmail.com>
2024-09-07 16:44:05 +0200

    update

--- receiver.py
+++ receiver.py
@@ -1,22 +1,69 @@
 import socket
 import pickle
 
-class EndOfStream:
-    pass
+class Receiver:
+    def __init__(self, host: str = 'localhost', port: int = 12345, timeout: float = 5.0) -> None:
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+        self.soket = None
+        self.connection = None
+        self.address = None
 
-def receive(handle_data: function, close_function: function = None) -> None:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('localhost', 12345))
-    s.listen(1)
-    conn, addr = s.accept()
-    print('Connessione accettata da', addr)
+    def __enter__(self) -> 'Receiver':
+        self.soket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.soket.settimeout(self.timeout)
+        self.soket.bind((self.host, self.port))
+        self.soket.listen(1)
+        try:
+            self.connection, self.address = self.soket.accept()
+        except socket.timeout as e:
+            raise TimeoutError("Connection timed out while waiting for a client") from e
+        return self
 
-    while True:
-        data = conn.recv(1024)
-        unpacked_data = pickle.loads(data)
-        if (isinstance(unpacked_data, EndOfStream)):
-            conn.close()
-            if close_function:
-                close_function()
-            return
-        handle_data(unpacked_data)
+    def receive(self) -> any:
+        try:
+            data = b""
+            while True:
+                packet = self.connection.recv(4096)
+                if not packet:  # No more data, stop receiving
+                    break
+                data += packet
+            return pickle.loads(data)  # Deserialize the received data
+        except (socket.error, pickle.UnpicklingError) as e:
+            raise ConnectionError("Failed to receive or decode data") from e
+
+    def close(self) -> None:
+        if self.connection:
+            self.connection.close()
+        if self.soket:
+            self.soket.close()
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        self.close()
+
+# Example usage:
+# with Receiver() as receiver:
+#     data = receiver.receive()
+#     print("Received:", data)
+
+
+# class EndOfStream:
+#     pass
+
+# def receive(handle_data: function, close_function: function = None) -> None:
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     s.bind(('localhost', 12345))
+#     s.listen(1)
+#     conn, addr = s.accept()
+#     print('Connessione accettata da', addr)
+
+#     while True:
+#         data = conn.recv(1024)
+#         unpacked_data = pickle.loads(data)
+#         if (isinstance(unpacked_data, EndOfStream)):
+#             conn.close()
+#             if close_function:
+#                 close_function()
+#             return
+#         handle_data(unpacked_data)
--- transmitter.py
+++ transmitter.py
@@ -1,14 +1,52 @@
 import socket
 import pickle
 
-class Transmitter:
-    def __init__(self) -> None:
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect(('localhost', 12345))
+import socket
+import pickle
 
-    def send(self, object) -> None:
-        message = pickle.dumps(object)
-        self.s.sendall(message)
-    
+class Transmitter:
+    def __init__(self, host: str = 'localhost', port: int = 12345, timeout: float = 5.0) -> None:
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+        self.soket = None
+
+    def __enter__(self) -> 'Transmitter':
+        self.soket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.soket.settimeout(self.timeout)
+        try:
+            self.soket.connect((self.host, self.port))
+        except socket.error as e:
+            raise ConnectionError(f"Failed to connect to {self.host}:{self.port}") from e
+        return self
+
+    def send(self, obj: any) -> None:
+        try:
+            message = pickle.dumps(obj)
+            self.soket.sendall(message)
+        except (socket.error, pickle.PicklingError) as e:
+            raise ConnectionError("Failed to send data") from e
+
     def close(self) -> None:
-        self.s.close()
+        if self.soket:
+            self.soket.close()
+
+    def __exit__(self) -> None:
+        self.close()
+
+# Example usage:
+# with Transmitter() as transmitter:
+#     transmitter.send({'key': 'value'})
+
+
+# class Transmitter:
+#     def __init__(self) -> None:
+#         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         self.s.connect(('localhost', 12345))
+
+#     def send(self, object) -> None:
+#         message = pickle.dumps(object)
+#         self.s.sendall(message)
+    
+#     def close(self) -> None:
+#         self.s.close()
