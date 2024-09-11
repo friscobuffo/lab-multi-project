71774a4b6b09fc0a3ef8b4a512859b81d1208b52
friscobuffo <gio.andreola@stud.uniroma3.it>
2024-09-11 14:06:11 +0200

    update compresser, aggiunte print transmitter dei bytes mandati

--- transmitter.py
+++ transmitter.py
@@ -5,6 +5,7 @@ from compresser import Compresser
 
 class Transmitter:
     def __init__(self):
+        self.total_sent = 0
         self.compresser = Compresser()
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect(('localhost', 9999))
@@ -15,8 +16,11 @@ class Transmitter:
             serialized_compressed = self.compresser.compress(serialized)
             compression_ratio = self.compresser.compute_compression_ratio(serialized, serialized_compressed)
             print(f"VLC compression ratio: {compression_ratio:.2f}")
+            self.total_sent += len(serialized_compressed)
             message = struct.pack("Q", len(serialized_compressed)) + serialized_compressed
             self.client_socket.sendall(message)
+            print(f"Sent {len(serialized_compressed)} bytes")
+            print(f"Total sent: {self.total_sent} bytes.")
         except (socket.error, pickle.PicklingError) as e:
             raise ConnectionError("Failed to send data") from e
     
