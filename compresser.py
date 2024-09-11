88edb32d50761c3fa1e73461d527f3cae72d295c
friscobuffo <gio.andreola@stud.uniroma3.it>
2024-09-11 14:14:50 +0200

    update libreria compressione usata

--- compresser.py
+++ compresser.py
@@ -1,11 +1,11 @@
-import zlib
+import lzma
     
 class Compresser:
     def compress(self, bytes_data: bytes) -> bytes:
-        return zlib.compress(bytes_data)
+        return lzma.compress(bytes_data)
     
     def decompress(self, encoded_bytes:bytes) -> bytes:
-        return zlib.decompress(encoded_bytes)
+        return lzma.decompress(encoded_bytes)
     
     def compute_compression_ratio(self, original_data: bytes, encoded_data: bytes):
         original_size = len(original_data)
